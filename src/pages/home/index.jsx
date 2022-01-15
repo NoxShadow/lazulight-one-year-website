e7b32159751708c8eeb0011f18aa313f0c6939cc
SG_bun <sgbunn3@gmail.com>
2022-01-15 14:01:51 -0500

    init removal of boilerplate

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default function HomePg(props) {
+  return (
+    <div
+      style={{
+        height: '100%',
+        width: '100%',
+      }}
+    >
+      <img
+        alt=''
+        src='/images/debut_bg.jpg'
+        style={{
+          height: '100%',
+          objectFit: 'cover',
+          width: '100%',
+        }}
+      />
+    </div>
+  );
+}
