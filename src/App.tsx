ef9f3f6a6e47d710ab70cb8959e5c65973db6980
ABuffSeagull <reecevanatta@hey.com>
2022-02-01 22:59:14 -0500

    Example of routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,17 @@
+import { Route, Switch, Link } from 'wouter'
+
 export default function App () {
-  return <div>Neat</div>
+  return (
+    <>
+      <Link to='/Elira'>To Elira</Link>
+      <Link to='/Finana'>To Finana</Link>
+      <Link to='/Pomu'>To Pomu</Link>
+      <Switch>
+        <Route path='/Elira'><div>Elira!</div></Route>
+        <Route path='/Finana'><div>Finana!</div></Route>
+        <Route path='/Pomu'><div>Pomu!</div></Route>
+        <Route><div>Whomst?</div></Route>
+      </Switch>
+    </>
+  )
 }
