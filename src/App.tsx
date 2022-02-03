a762ba94771796e388e9344c83e88eae143f3940
ABuffSeagull <reecevanatta@hey.com>
2022-02-02 22:04:40 -0500

    Show off CSS modules a bit more

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import { Route, Switch, Link } from 'wouter'
+import classes from './App.module.scss'
+import Elira from './Elira'
 
 export default function App () {
   return (
-    <>
-      <Link to='/Elira'>To Elira</Link>
-      <Link to='/Finana'>To Finana</Link>
-      <Link to='/Pomu'>To Pomu</Link>
+    <div className={classes.container}>
+      <nav>
+        <Link className={classes.link} to='/Finana'>To Finana</Link>
+        <Link className={classes.link} to='/Pomu'>To Pomu</Link>
+        <Link className={classes.link} to='/Elira'>To Elira</Link>
+      </nav>
       <Switch>
-        <Route path='/Elira'><div>Elira!</div></Route>
+        <Route path='/Elira'><Elira /></Route>
         <Route path='/Finana'><div>Finana!</div></Route>
         <Route path='/Pomu'><div>Pomu!</div></Route>
         <Route><div>Whomst?</div></Route>
       </Switch>
-    </>
+    </div>
   )
 }
